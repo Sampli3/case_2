cd41685367ecb774237f78c0bb1725addca509cd
alenatorgasheva <in-fa@mail.ru>
2019-10-14 19:59:01 +0700

    Added localization and checked PEP8.

---
+++ dataforsingleparent.py
@@ -0,0 +1,15 @@
+# There are monetary ranges for single parent.
+
+first_stage_1 = 0
+first_stage_2 = 12950           # 10%
+second_stage_1 = 12951
+second_stage_2 = 49400          # 15%
+third_stage_1 = 49401
+third_stage_2 = 127550          # 25%
+fourth_stage_1 = 127551
+fourth_stage_2 = 206600         # 28%
+fifth_stage_1 = 206601
+fifth_stage_2 = 405100          # 33%
+sixth_stage_1 = 405101
+sixth_stage_2 = 432200          # 35%
+seventh_stage = 432201          # 39.6%
